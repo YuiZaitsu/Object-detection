c087433a5acb9c7c71ff7befb2ec73f21bc09b8d
Yutsuki Zaitsu <89077321+YuiZaitsu@users.noreply.github.com>
2023-04-17 12:18:40 +0530

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,42 @@
+img="";
+
+
+function preload(){
+img=loadImage("FA.jpg");
+}
+
+function draw(){
+image(img,0,0,640,420);
+fill("red");
+stroke("red");
+text("Fan",45,75);
+noFill();
+
+rect(30,60,450,350);
+
+}
+
+ function setup(){
+ canvas=createCanvas(640,420);
+ canvas.center();
+
+ }
+ function ac()
+{
+   
+    window.location = "ac.html";
+    
+}
+function bedroom()
+{
+   
+    window.location = "bedroom.html";
+    
+}
+
+function fan()
+{
+   
+    window.location = "fan.html";
+    
+}
