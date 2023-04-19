002236286c6ebf9507e3a6748b3c169e6c0bd776
Yutsuki Zaitsu <89077321+YuiZaitsu@users.noreply.github.com>
2023-04-19 14:09:53 +0530

    Add files via upload

--- fan.js
+++ fan.js
@@ -1,24 +1,33 @@
 img="";
 
+status="";
+objects= [];
 
 function preload(){
 img=loadImage("FA.jpg");
 }
 
 function draw(){
-image(img,0,0,640,420);
-fill("red");
-stroke("red");
-text("Fan",45,75);
-noFill();
-
-rect(30,60,450,350);
-
+    image(img,0,0,640,420);
+    if(status!="")
+    {
+       for(i=0; i<=objects.length; i++){
+          document.getElementById("status").innerHTML="status: Objects detected";
+          fill("red");
+          percent=floor(objects[i].confidence*100);
+          text(objects[i].label+" "+percent+"%", objects[i].x, objects[i].y);
+          noFill();
+          stroke("red");
+          rect( objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+       }
+    }
 }
 
  function setup(){
  canvas=createCanvas(640,420);
  canvas.center();
+ objectdetector=ml5.objectDetector("cocossd",modelloaded);
+document.getElementById("status").innerHTML="status: detecting objects";
 
  }
  function ac()
@@ -40,3 +49,21 @@ function fan()
     window.location = "fan.html";
     
 }
+
+function modelloaded(){
+    console.log("Model Is Loaded");
+    status=true;
+
+    objectdetector.detect(img,gotresult);
+
+ }
+
+ function gotresult(error,results)
+ {
+    if(error){
+console.log(error);
+
+    }
+  console.log(results);
+    objects=results;
+ }
